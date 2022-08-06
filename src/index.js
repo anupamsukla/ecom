12d6a6f445b4cadb5d7d5a35667dbe3a943b30a9
anupamsukla <32832216+anupamsukla@users.noreply.github.com>
2022-08-06 18:13:28 +0530

    server deployment

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-import "./index.css";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Provider as ReduxProvider } from "react-redux";
-import { store } from "./redux/store/store";
-import axios from "axios";
-import { theme } from "./theme";
+import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider as ReduxProvider } from 'react-redux'
+import { store } from './redux/store/store'
+import axios from 'axios'
+import { theme } from './theme'
 
-import reportWebVitals from "./reportWebVitals";
-axios.defaults.baseURL = "https://e-store-01.herokuapp.com/";
+import reportWebVitals from './reportWebVitals'
+axios.defaults.baseURL = 'https://ecomerce101.herokuapp.com/'
 
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
@@ -24,10 +23,10 @@ root.render(
         </Router>
       </ReduxProvider>
     </ChakraProvider>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
